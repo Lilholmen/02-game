0e96b04242fe2af41c378da577debe3ef921a0ae
Lilholmen <lilholmen@yandex.ru>
2023-01-09 11:55:56 +0300

    del waste css files; add index.html to tailwing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     fontFamily: {
-      sans: ["Anek Latin", "sans-serif"],
+      sans: ['Anek Latin', 'sans-serif'],
     },
     extend: {
       width: {
-        min: "1px",
+        min: '1px',
       },
       borderRadius: {
-        large: "64px",
+        large: '64px',
       },
     },
   },
