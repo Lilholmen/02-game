ce569407b32f7a31bac301953ead8eb20adf1d76
Artur Yandulskii <art.yan.dev@gmail.com>
2023-01-12 19:48:32 +0300

    created footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="z-50 flex w-full justify-between bg-stone-700 py-1 px-4 font-sans-modal text-sm text-stone-400">
+      <div>2023 / project 2</div>
+      <div>CDG Academy</div>
+      <div>by Artur Yandulskii</div>
+    </footer>
+  );
+};
+
+export default Footer;
